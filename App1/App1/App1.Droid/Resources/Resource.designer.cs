98b115ab701950301da15c65cbf5b879e9cfd1a4
Oguzhan Cihan <0911315@hr.nl>
2016-06-27 16:07:32 +0200

    PieChart Demo

--- App1/App1/App1.Droid/Resources/Resource.designer.cs
+++ App1/App1/App1.Droid/Resources/Resource.designer.cs
@@ -109,6 +109,9 @@ namespace App1.Droid
 			// aapt resource value: 0x7f030001
 			public const int Main = 2130903041;
 			
+			// aapt resource value: 0x7f030002
+			public const int pieChart = 2130903042;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
--- App1/App1/App1.Droid/pieChart.cs
+++ App1/App1/App1.Droid/pieChart.cs
@@ -9,6 +9,10 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using OxyPlot.Xamarin.Android;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace App1.Droid
 {
@@ -18,8 +22,25 @@ namespace App1.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.pieChart);
+            PlotView view = FindViewById<PlotView>(Resource.Id.plot_view);
+            view.Model = CreatePlotModel();
+        }
+        public PlotModel CreatePlotModel()
+        {
+            var modelP1 = new PlotModel { Title = "Most stolen bike brands" };
 
-            // Create your application here
+            var seriesP1 = new PieSeries { StrokeThickness = 2.0, InsideLabelPosition = 0.8, AngleSpan = 360, StartAngle = 0 };
+
+            seriesP1.Slices.Add(new PieSlice("Giant", 1030) { IsExploded = false, Fill = OxyColors.PaleVioletRed });
+            seriesP1.Slices.Add(new PieSlice("Batavus", 929) { IsExploded = true });
+            seriesP1.Slices.Add(new PieSlice("Gazelle", 4157) { IsExploded = true });
+            seriesP1.Slices.Add(new PieSlice("Piagio", 739) { IsExploded = true });
+            seriesP1.Slices.Add(new PieSlice("Holland", 35) { IsExploded = true });
+
+            modelP1.Series.Add(seriesP1);
+
+            return modelP1;
         }
     }
 }
