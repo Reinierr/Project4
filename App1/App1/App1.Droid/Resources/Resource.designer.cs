5829115d6dbb7283a4a2a15153dee59a815a45a9
Marcel <Marcel_stassen@hotmail.com>
2016-06-27 14:49:34 +0200

    Add LineChart

--- App1/App1/App1.Droid/Resources/Resource.designer.cs
+++ App1/App1/App1.Droid/Resources/Resource.designer.cs
@@ -72,20 +72,23 @@ namespace App1.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int barchart = 2131034113;
-			
-			// aapt resource value: 0x7f050000
-			public const int layoutContainer = 2131034112;
-			
 			// aapt resource value: 0x7f050002
-			public const int lineChart = 2131034114;
+			public const int barchart = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int layoutContainer = 2131034113;
 			
 			// aapt resource value: 0x7f050003
-			public const int myButton = 2131034115;
+			public const int lineChart = 2131034115;
 			
 			// aapt resource value: 0x7f050004
-			public const int pieChart = 2131034116;
+			public const int myButton = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int pieChart = 2131034117;
+			
+			// aapt resource value: 0x7f050000
+			public const int plot_view = 2131034112;
 			
 			static Id()
 			{
@@ -101,7 +104,10 @@ namespace App1.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int lineChart = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
--- App1/App1/App1.Droid/lineChart.cs
+++ App1/App1/App1.Droid/lineChart.cs
@@ -9,6 +9,10 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using OxyPlot.Xamarin.Android;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace App1.Droid
 {
@@ -18,8 +22,62 @@ namespace App1.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.lineChart);
+            PlotView view = FindViewById<PlotView>(Resource.Id.plot_view);
+            view.Model = CreatePlotModel();
+        }
 
-            // Create your application here
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2140:TransparentMethodsMustNotReferenceCriticalCodeFxCopRule")]
+        protected PlotModel CreatePlotModel()
+        {
+            string title = "fietsdiefstallen per maand";
+            var plotModel = new PlotModel
+            {
+                Title = title,
+                TitleColor = OxyColors.White,
+                TitleFontSize = 24,
+                LegendPlacement = LegendPlacement.Inside,
+                LegendPosition = LegendPosition.TopRight,
+                LegendOrientation = LegendOrientation.Horizontal,
+                LegendBorderThickness = 0
+            };
+            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Title = "Maanden", TitleFontSize = 20, TitleColor = OxyColors.White });
+            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Maximum = 10, Minimum = 0, Title = "diefstal", TitleColor = OxyColors.White, TitleFontSize = 20, AxislineColor = OxyColors.White });
+
+
+            var series1 = new LineSeries
+            {
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 4,
+                MarkerStroke = OxyColors.White
+            };
+            Random y = new Random();
+            for (Double i = 0; i < 13; i++)
+            {
+                Double z = y.Next(0, 11);
+                series1.Points.Add(new DataPoint(i, z+1));
+            }
+            var series2 = new LineSeries
+            {
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 4,
+                MarkerStroke = OxyColors.Yellow,
+
+
+            };
+            Random h = new Random();
+            for (Double i = 0; i < 13; i++)
+            {
+
+                Double z = h.Next(0, 11);
+                series2.Points.Add(new DataPoint(i, z-1));
+            }
+
+            plotModel.Series.Add(series1);
+            plotModel.Series.Add(series2);
+
+
+            return plotModel;
         }
     }
 }
