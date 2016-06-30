6eef1e6de18a5390d5bc29adcfb4e286bfda3702
rodofloni <romanr@hotmail.nl>
2016-06-30 10:29:48 +0100

    push for sync

--- App1/App1/App1.Droid/savebike.cs
+++ App1/App1/App1.Droid/savebike.cs
@@ -9,14 +9,19 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Provider;
 
 namespace App1.Droid
 {
     [Activity(Label = "savebike", Icon = "@drawable/icon")]
     public class savebike : Activity
-    {
+    {   
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+   
+
             base.OnCreate(savedInstanceState);
 
             // Create your application here
